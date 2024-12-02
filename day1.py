4ec946d73402c885425fbc56e5ae80bdac0dfe09
David Tyler <davidtyler@microsoft.com>
2024-12-02 10:44:21 +0000

    Added day 1 and day 2

---
+++ day1.py
@@ -0,0 +1,37 @@
+import math
+
+def run(input_file: str):
+  with open(input_file, "r") as input_file:
+    lines = input_file.readlines()
+
+  list_1 = []
+  list_2 = []
+  counts = {}
+  for split_line in [line.split(" ") for line in lines]:
+    list_1.append(int(split_line[0]))
+    list_2.append(int(split_line[-1]))
+
+    if int(split_line[-1]) in counts:
+      counts[int(split_line[-1])] += 1
+    else:
+      counts[int(split_line[-1])] = 1
+
+  list_1.sort()
+  list_2.sort()
+
+  total = 0
+  for x, y in zip(list_1, list_2):
+    total += abs(x - y)
+
+  print(total)
+
+  part2 = 0
+  for x in list_1:
+    if x in counts:
+      part2 += x*counts[x]
+
+  print(part2)
+
+if __name__ == "__main__":
+  run("test/day1")
+  run("input/day1")
---
+++ day2.py
@@ -0,0 +1,48 @@
+def check_parts(parts):
+  increasing = parts[0] < parts[1]
+
+  safe = True
+  compare_part = parts[0]
+  for part in parts[1:]:
+    if increasing:
+      if compare_part < part and part - compare_part < 4:
+        compare_part = part
+      else:
+        safe = False
+        break
+    else:
+      if compare_part > part and compare_part - part < 4:
+        compare_part = part
+      else:
+        safe = False
+        break
+
+  return safe
+
+def run(input_file: str):
+  with open(input_file) as day2:
+    lines = day2.readlines()
+
+  count_part_1 = 0
+  count_part_2 = 0
+  for line in lines:
+    parts = [int(p) for p in line.split(" ")]
+
+    all_safe = check_parts(parts)
+      
+    if all_safe:
+      count_part_1 += 1
+      count_part_2 += 1
+    else:
+      for i in range(0, len(parts)):
+        if check_parts(parts[:i] + parts[i+1:]):
+          count_part_2 += 1
+          break
+
+  print(count_part_1)
+  print(count_part_2)
+
+
+if __name__ == "__main__":
+  run("test/day2")
+  run("input/day2")
